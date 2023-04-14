210306fa1e007309776eff205ae58361d4378cd3
persival001 <persival@free.fr>
2023-04-14 15:39:16 +0200

    First Commit

--- app/src/main/java/com/persival/countryname/MainActivity.java
+++ app/src/main/java/com/persival/countryname/MainActivity.java
@@ -1,14 +1,57 @@
 package com.persival.countryname;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.persival.countryname.model.CountryModel;
+import com.persival.countryname.model.Result;
+import com.persival.countryname.service.GetCountryDataService;
+import com.persival.countryname.service.RetrofitInstance;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<CountryModel> countries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getCountries();
+    }
+
+    public Object getCountries() {
+        GetCountryDataService getCountryDataService = RetrofitInstance.getService();
+        Call<Result> call = getCountryDataService.getResult();
+
+        call.enqueue(new Callback<Result>() {
+            @Override
+            public void onResponse(Call<Result> call, Response<Result> response) {
+
+                if(response.body() != null && response.body().getResult() != null) {
+                    countries = (ArrayList<CountryModel>) response.body().getResult();
+
+                    for(CountryModel country : countries) {
+                        Log.i("Country", "" + country.getName());
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Result> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return countries;
     }
 }
